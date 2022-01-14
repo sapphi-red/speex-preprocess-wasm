2e73751461c631a27b08fd19ca668f984c8b76ac
sapphi-red <green@sapphi.red>
2022-01-14 22:43:24 +0900

    Add types

---
+++ src/SpeexPreprocessCtlRequest.ts
@@ -0,0 +1,120 @@
+export const SpeexPreprocessCtlRequest = {
+  /** Set preprocessor denoiser state */
+  SET_DENOISE: 0,
+  /** Get preprocessor denoiser state */
+  GET_DENOISE: 1,
+
+  /** Set preprocessor Automatic Gain Control state */
+  SET_AGC: 2,
+  /** Get preprocessor Automatic Gain Control state */
+  GET_AGC: 3,
+
+  /** Set preprocessor Voice Activity Detection state */
+  SET_VAD: 4,
+  /** Get preprocessor Voice Activity Detection state */
+  GET_VAD: 5,
+
+  /** Set preprocessor Automatic Gain Control level (float) */
+  SET_AGC_LEVEL: 6,
+  /** Get preprocessor Automatic Gain Control level (float) */
+  GET_AGC_LEVEL: 7,
+
+  /** Set preprocessor dereverb state */
+  SET_DEREVERB: 8,
+  /** Get preprocessor dereverb state */
+  GET_DEREVERB: 9,
+
+  /** Set preprocessor dereverb level */
+  SET_DEREVERB_LEVEL: 10,
+  /** Get preprocessor dereverb level */
+  GET_DEREVERB_LEVEL: 11,
+
+  /** Set preprocessor dereverb decay */
+  SET_DEREVERB_DECAY: 12,
+  /** Get preprocessor dereverb decay */
+  GET_DEREVERB_DECAY: 13,
+
+  /** Set probability required for the VAD to go from silence to voice */
+  SET_PROB_START: 14,
+  /** Get probability required for the VAD to go from silence to voice */
+  GET_PROB_START: 15,
+
+  /** Set probability required for the VAD to stay in the voice state (integer percent) */
+  SET_PROB_CONTINUE: 16,
+  /** Get probability required for the VAD to stay in the voice state (integer percent) */
+  GET_PROB_CONTINUE: 17,
+
+  /** Set maximum attenuation of the noise in dB (negative number) */
+  SET_NOISE_SUPPRESS: 18,
+  /** Get maximum attenuation of the noise in dB (negative number) */
+  GET_NOISE_SUPPRESS: 19,
+
+  /** Set maximum attenuation of the residual echo in dB (negative number) */
+  SET_ECHO_SUPPRESS: 20,
+  /** Get maximum attenuation of the residual echo in dB (negative number) */
+  GET_ECHO_SUPPRESS: 21,
+
+  /** Set maximum attenuation of the residual echo in dB when near end is active (negative number) */
+  SET_ECHO_SUPPRESS_ACTIVE: 22,
+  /** Get maximum attenuation of the residual echo in dB when near end is active (negative number) */
+  GET_ECHO_SUPPRESS_ACTIVE: 23,
+
+  /** Set the corresponding echo canceller state so that residual echo suppression can be performed (NULL for no residual echo suppression) */
+  SET_ECHO_STATE: 24,
+  /** Get the corresponding echo canceller state */
+  GET_ECHO_STATE: 25,
+
+  /** Set maximal gain increase in dB/second (int32) */
+  SET_AGC_INCREMENT: 26,
+
+  /** Get maximal gain increase in dB/second (int32) */
+  GET_AGC_INCREMENT: 27,
+
+  /** Set maximal gain decrease in dB/second (int32) */
+  SET_AGC_DECREMENT: 28,
+
+  /** Get maximal gain decrease in dB/second (int32) */
+  GET_AGC_DECREMENT: 29,
+
+  /** Set maximal gain in dB (int32) */
+  SET_AGC_MAX_GAIN: 30,
+
+  /** Get maximal gain in dB (int32) */
+  GET_AGC_MAX_GAIN: 31,
+
+  /*  Can't set loudness */
+  /** Get loudness */
+  GET_AGC_LOUDNESS: 33,
+
+  /*  Can't set gain */
+  /** Get current gain (int32 percent) */
+  GET_AGC_GAIN: 35,
+
+  /*  Can't set spectrum size */
+  /** Get spectrum size for power spectrum (int32) */
+  GET_PSD_SIZE: 37,
+
+  /*  Can't set power spectrum */
+  /** Get power spectrum (int32[] of squared values) */
+  GET_PSD: 39,
+
+  /*  Can't set noise size */
+  /** Get spectrum size for noise estimate (int32)  */
+  GET_NOISE_PSD_SIZE: 41,
+
+  /*  Can't set noise estimate */
+  /** Get noise estimate (int32[] of squared values) */
+  GET_NOISE_PSD: 43,
+
+  /* Can't set speech probability */
+  /** Get speech probability in last frame (int32).  */
+  GET_PROB: 45,
+
+  /** Set preprocessor Automatic Gain Control level (int32) */
+  SET_AGC_TARGET: 46,
+  /** Get preprocessor Automatic Gain Control level (int32) */
+  GET_AGC_TARGET: 47
+} as const
+
+export type SpeexPreprocessCtlRequest =
+  typeof SpeexPreprocessCtlRequest[keyof typeof SpeexPreprocessCtlRequest]
---
+++ wasm-out/speex.d.ts
@@ -0,0 +1,45 @@
+export type F32Ptr = number
+export type SpeexPreprocessState = number
+export type SpeexPreprocessCtlRequest = number
+
+export interface SpeexModule extends EmscriptenModule {
+  /**
+   * Creates a new preprocessing state. You MUST create one state per channel processed.
+   * @param frame_size Number of samples to process at one time (should correspond to 10-20 ms). Must be
+   * the same value as that used for the echo canceller for residual echo cancellation to work.
+   * @param sampling_rate Sampling rate used for the input.
+   * @return Newly created preprocessor state
+   */
+  _speex_preprocess_state_init(
+    frame_size: number,
+    sampling_rate: number
+  ): SpeexPreprocessState
+  /**
+   * Destroys a preprocessor state
+   * @param st Preprocessor state to destroy
+   */
+  _speex_preprocess_state_destroy(state: SpeexPreprocessState): void
+  /**
+   * Preprocess a frame
+   * @param st Preprocessor state
+   * @param x Audio sample vector (in and out). Must be same size as specified in speex_preprocess_state_init().
+   * @return Bool value for voice activity (1 for speech, 0 for noise/silence), ONLY if VAD turned on.
+   */
+  _speex_preprocess_run(state: SpeexPreprocessState, x: F32Ptr): 1 | 0
+  /**
+   * Used like the ioctl function to control the preprocessor parameters
+   * @param st Preprocessor state
+   * @param request ioctl-type request (one of the SPEEX_PREPROCESS_* macros)
+   * @param ptr Data exchanged to-from function
+   * @return 0 if no error, -1 if request in unknown
+   */
+  _speex_preprocess_ctl(
+    state: SpeexPreprocessState,
+    request: SpeexPreprocessCtlRequest,
+    void_: unknown
+  ): -1 | 0
+}
+
+declare const loadModule: EmscriptenModuleFactory<SpeexModule>
+
+export default loadModule
