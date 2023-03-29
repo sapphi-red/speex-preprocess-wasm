8e7d742a44bf9f769ee280f94b7c4b21902d4f02
sapphi-red <green@sapphi.red>
2023-03-29 13:30:30 +0900

    chore: remove USE_ES6_IMPORT_META=0

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   target: 'es2019',
+  esbuildOptions(options) {
+    options.supported ??= {}
+    options.supported['import-meta'] = true
+  },
   platform: 'browser',
   sourcemap: true,
   format: ['esm', 'cjs'],
